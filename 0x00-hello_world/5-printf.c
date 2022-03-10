6dd4c5e7bbf429cfa22042a4ef8d0b49783fa51b
Machoka Daisy <machokadaisy25@gmail.com>
2022-03-10 14:42:28 -0800

    the rest pt two

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  *  *  main - main function
@@ -8,4 +7,4 @@ int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
-}
+
