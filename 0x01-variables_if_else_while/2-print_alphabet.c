eab45ef13456bc4296852293ee109231582c1349
Machoka Daisy <machokadaisy25@gmail.com>
2022-03-11 18:57:59 -0800

    trials

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase
+ * main - prints the 
+ * alphabet in lowercase
  *  followed by a new line
  *   Return: Always 0 (Success)
  */
@@ -10,7 +11,7 @@ int main(void)
 {
 	char gru;
 
-	for (gru = 'a'; gru <= 'z'; gru++)
+	for (gru ='a'; gru <= 'z'; gru++)
 	{
 		putchar(gru);
 	}
