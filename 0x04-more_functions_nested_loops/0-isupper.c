27fc08eb11929eed0122009a368fb315f9e17025
Machoka Daisy <machokadaisy25@gmail.com>
2022-03-17 14:15:43 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * isupper - checks for uppercase character
+ * @c: the character to bee checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
