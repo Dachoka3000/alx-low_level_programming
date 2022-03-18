70573234f8db65346682286c8446400ca2a18016
Machoka Daisy <machokadaisy25@gmail.com>
2022-03-17 18:56:00 -0700

    sample tree

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,24 +6,26 @@
  */
 void print_diagonal(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	} else 
-	{
-		int i, j;
+	int sla;
+	int space;
 
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-					putchar('\\');
-				else if (j < i)
-					_putchar(' ');
-			}
-		}
-		_putchar('\n');
+	if (n <=0)
+	{
+		_putchar ('n');
 	}
+	else
+	{
+		for (sla = 1; sla <= n; sla++)
+		{
+			for (space = 1; space < sla; space++)
+			{
+				_putchar (' ');
+			}
+			_putchar ('\\');
+			_putchar ('\n');
+		}
+
+	}
+
 }
 
